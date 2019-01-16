370ed6b9c6fbe970c138bac08825eb1ba7871497
ebuchheim <emma.buchheim@gmail.com>
2019-01-16 10:39:38 -0800

    add method logic

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,30 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(dwarves)# code an argument here
   # Your code here
+  length = dwarves.count
+  i = 0
+    while i < length
+      puts "#{i +1} #{dwarves[i]}"
+      i += 1
+    end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(planets)# code an argument here
   # Your code here
+  planets.map {|planet| "#{planet.capitalize}!"}
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(calls)# code an argument here
   # Your code here
+  calls.any?{|call| call.length > 4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(list)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  list.each do |item|
+    if cheese_types.include?(item)
+      return item
+    end
+  end
+  nil
 end
